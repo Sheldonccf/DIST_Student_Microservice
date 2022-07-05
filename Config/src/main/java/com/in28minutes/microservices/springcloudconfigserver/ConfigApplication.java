10e26aa917582472d997d4684be65d02d533d6b7
Sheldon Changfeng Chen <89536920+Sheldonccf@users.noreply.github.com>
2022-07-05 03:31:37 -0400

    test-service connect to Spring Cloud Config

---
+++ Config/src/main/java/com/in28minutes/microservices/springcloudconfigserver/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
---
+++ Test-service/src/main/java/com/in28minutes/microservices/testservice/Configuration.java
@@ -0,0 +1,30 @@
+package com.in28minutes.microservices.testservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("test-service")
+public class Configuration {
+
+
+    private int maximum;
+    private int minimum;
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+}
