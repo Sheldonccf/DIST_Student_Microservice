1eb94d6790e89a2792b03fb807ad7178f154725b
Sheldon Changfeng Chen <89536920+Sheldonccf@users.noreply.github.com>
2022-07-06 01:19:13 -0400

    test-service connect to Spring Cloud Config

---
+++ test-service-2/src/main/java/com/in28minutes/microservices/testservice2/TestService2Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.testservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestService2Application.class, args);
+    }
+
+}
