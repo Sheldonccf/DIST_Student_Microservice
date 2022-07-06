d3d1594f75d30dc8a208cbad2cb5bb8c2f4e5d80
Sheldon Changfeng Chen <89536920+Sheldonccf@users.noreply.github.com>
2022-07-06 01:18:57 -0400

    test-service connect to Spring Cloud Config

---
+++ test-service-2/src/test/java/com/in28minutes/microservices/testservice2/TestService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.testservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
